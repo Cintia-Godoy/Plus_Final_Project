ddd9534ea68f98e03f9f2edf2aa8b2477017f64c
Cintia-Godoy <137214626+Cintia-Godoy@users.noreply.github.com>
2024-07-11 14:28:50 +0100

    created api for city/temperature search

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
+function searchCity(city) {
+  let apiKey = "a2ft6b2f4980fb5816664553c2ofa3c0";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  console.log(searchInput.value);
+
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+searchCity("London");
